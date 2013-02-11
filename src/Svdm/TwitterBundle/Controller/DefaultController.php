931e9debd032c8051f514431138079a78b9838c1
jasonblanchard <jas.blanchard@gmail.com>
2013-02-10 19:57:07 -0500

    Moved some app logic into lib.php

--- src/Svdm/TwitterBundle/Controller/DefaultController.php
+++ src/Svdm/TwitterBundle/Controller/DefaultController.php
@@ -5,6 +5,8 @@ namespace Svdm\TwitterBundle\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Response;
 
+require_once(dirname(__FILE__) . '/lib.php');
+
 class DefaultController extends Controller
 {
     public function indexAction() {
@@ -20,8 +22,6 @@ class DefaultController extends Controller
       $tweets = make_twitter_objects($tweet_json);
 
       $timed_tweets = group_tweets($tweets);
-
-      #print_r($timed_tweets);
 
       $final_tweet = false;
 
@@ -47,99 +47,4 @@ class DefaultController extends Controller
 
       */
     }
-}
-
-class TwitterMessage {
-
-  public $tid = '';
-
-  public $message = '';
-
-  public $timestamp = '';
-
-  public $media_url = '';
-
-  function __construct($tid, $message, $timestamp, $media_url) {
-    $this->tid = $tid;
-    $this->message = $message;
-    $this->timestamp = $timestamp;
-    $this->media_url = $media_url;
-  }
-  
-}
-
-function get_tweet_json($url) {
-  $json = file_get_contents($url);
-
-  $cache = fopen(dirname(__FILE__) . "/tweet_cache.js", 'r+');
-
-  fwrite($cache, $json);
-
-  fclose($cache);
-  
-  return json_decode($json, TRUE);
-}
-
-function get_tweet_json_from_cache() {
-  
-  $filename = dirname(__FILE__) . "/tweet_cache.js";
-
-  $cache = fopen($filename, 'r+');
-
-  $json = fread($cache, filesize($filename));
-
-  return json_decode($json, TRUE);
-}
-
-function make_twitter_objects($json) {
-
-  $tweets = array();
-
-  foreach ($json as $tweet) {
-    $tid = $tweet['id_str'];
-    $message = $tweet['text'];
-    $timestamp = $tweet['created_at'];
-
-    if (isset($tweet['entities']['media'])) {
-      $media_url = $tweet['entities']['media'][0]['media_url'];
-    } else {
-      $media_url = '';
-    }
-
-    $tweet_obj = new TwitterMessage($tid, $message, $timestamp, $media_url);
-
-    $tweets[] = $tweet_obj;
-  }
-
-  return $tweets;
-}
-
-function group_tweets($tweet_array) {
-  $now = time();
-  $newest = array();
-  $older = array();
-  $tweet_time_array = array();
-
-  foreach ($tweet_array as $tweet) {
-    if (strtotime($tweet->timestamp) > ($now - 60)) {
-      $newest[] = $tweet;
-    } else {
-      $older[] = $tweet;
-    }
-  }
-
-  $tweet_time_array["newest"] = $newest;
-  $tweet_time_array["older"] = $older;
-
-  return $tweet_time_array;
-}
-
-function pick_tweet($tweets, $blacklist) {
-  $random = rand(0, count($tweets) - 1);
-
-  if (in_array($tweets[$random]->tid, $blacklist)) {
-    return false;
-  } else {
-    return $tweets[$random];
-  }
 }
---
+++ src/Svdm/TwitterBundle/Controller/lib.php
@@ -0,0 +1,96 @@
+<?php
+
+class TwitterMessage {
+
+  public $tid = '';
+
+  public $message = '';
+
+  public $timestamp = '';
+
+  public $media_url = '';
+
+  function __construct($tid, $message, $timestamp, $media_url) {
+    $this->tid = $tid;
+    $this->message = $message;
+    $this->timestamp = $timestamp;
+    $this->media_url = $media_url;
+  }
+  
+}
+
+function get_tweet_json($url) {
+  $json = file_get_contents($url);
+
+  $cache = fopen(dirname(__FILE__) . "/tweet_cache.js", 'r+');
+
+  fwrite($cache, $json);
+
+  fclose($cache);
+  
+  return json_decode($json, TRUE);
+}
+
+function get_tweet_json_from_cache() {
+  
+  $filename = dirname(__FILE__) . "/tweet_cache.js";
+
+  $cache = fopen($filename, 'r+');
+
+  $json = fread($cache, filesize($filename));
+
+  return json_decode($json, TRUE);
+}
+
+function make_twitter_objects($json) {
+
+  $tweets = array();
+
+  foreach ($json as $tweet) {
+    $tid = $tweet['id_str'];
+    $message = $tweet['text'];
+    $timestamp = $tweet['created_at'];
+
+    if (isset($tweet['entities']['media'])) {
+      $media_url = $tweet['entities']['media'][0]['media_url'];
+    } else {
+      $media_url = '';
+    }
+
+    $tweet_obj = new TwitterMessage($tid, $message, $timestamp, $media_url);
+
+    $tweets[] = $tweet_obj;
+  }
+
+  return $tweets;
+}
+
+function group_tweets($tweet_array) {
+  $now = time();
+  $newest = array();
+  $older = array();
+  $tweet_time_array = array();
+
+  foreach ($tweet_array as $tweet) {
+    if (strtotime($tweet->timestamp) > ($now - 60)) {
+      $newest[] = $tweet;
+    } else {
+      $older[] = $tweet;
+    }
+  }
+
+  $tweet_time_array["newest"] = $newest;
+  $tweet_time_array["older"] = $older;
+
+  return $tweet_time_array;
+}
+
+function pick_tweet($tweets, $blacklist) {
+  $random = rand(0, count($tweets) - 1);
+
+  if (in_array($tweets[$random]->tid, $blacklist)) {
+    return false;
+  } else {
+    return $tweets[$random];
+  }
+}
