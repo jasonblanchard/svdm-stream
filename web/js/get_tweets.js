f2517350f911d4095faa5aaf554585669e9de4a4
jasonblanchard <jas.blanchard@gmail.com>
2013-02-12 08:54:43 -0500

    Extracted js functions and added a timer

--- web/js/get_tweets.js
+++ web/js/get_tweets.js
@@ -1,25 +1,38 @@
 $(document).ready( function() {
 
+  var blacklist1 = '';
+  var blacklist2 = '';
+  var blacklist3 = '';
+
   var requestURL = 'http://dev-site.vbox.local/symfony/web/app_dev.php/svdm/tweet?1=' + blacklist1 + "&2=" + blacklist2 + "&3=" + blacklist3;
 
   console.log(requestURL);
 
+  get_tweet(requestURL);
+
+  //setInterval(function() { get_tweet(requestURL)}, 1000);
+
+});
+
+
+function get_tweet(url) {
   $.ajax({
     type: 'GET',
-    url: requestURL,
+    url: url,
     success: function(data) { insertTweet(data); },
     contentType: "application/json",
     dataType: 'json'
   });
+}
 
-  function insertTweet(tweet) {
 
-    $('.tweet-message').text(tweet.message);
+function insertTweet(tweet) {
 
-    if (tweet.media_url != "") {
-      $('.tweet-media').html("<img src='" + tweet.media_url + "'>");
-    }
+  $('.tweet-media').html('');
+
+  $('.tweet-message').text(tweet.message);
+
+  if (tweet.media_url != "") {
+    $('.tweet-media').html("<img src='" + tweet.media_url + "'>");
   }
-
-
-});
+}
