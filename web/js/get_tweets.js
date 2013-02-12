92bbe7e511c8a6df0339633f3aa93d5b94652a74
jasonblanchard <jas.blanchard@gmail.com>
2013-02-12 09:24:12 -0500

    Seperated initURL from the reqeuestURL

--- web/js/get_tweets.js
+++ web/js/get_tweets.js
@@ -1,38 +1,41 @@
 $(document).ready( function() {
 
-  var blacklist1 = '';
-  var blacklist2 = '';
-  var blacklist3 = '';
+  var blacklist = ['','','']
+  var requestURL;
 
-  var requestURL = 'http://dev-site.vbox.local/symfony/web/app_dev.php/svdm/tweet?1=' + blacklist1 + "&2=" + blacklist2 + "&3=" + blacklist3;
+  var initURL = 'http://dev-site.vbox.local/symfony/web/app_dev.php/svdm/tweet'
 
-  console.log(requestURL);
-
-  get_tweet(requestURL);
+  get_tweet(initURL);
 
   //setInterval(function() { get_tweet(requestURL)}, 1000);
 
+  function get_tweet(url) {
+    $.ajax({
+      type: 'GET',
+      url: url,
+      success: function(data) { insertTweet(data); },
+      contentType: "application/json",
+      dataType: 'json'
+    });
+
+    console.log(url);
+  }
+
+
+  function insertTweet(tweet) {
+
+    $('.tweet-media').html('');
+
+    $('.tweet-message').text(tweet.message);
+
+    if (tweet.media_url != "") {
+      $('.tweet-media').html("<img src='" + tweet.media_url + "'>");
+    }
+
+    blacklist[0] = tweet.tid;
+
+    requestURL = 'http://dev-site.vbox.local/symfony/web/app_dev.php/svdm/tweet?1=' + blacklist[0] + "&2=" + blacklist[1] + "&3=" + blacklist[2];
+  }
 });
 
 
-function get_tweet(url) {
-  $.ajax({
-    type: 'GET',
-    url: url,
-    success: function(data) { insertTweet(data); },
-    contentType: "application/json",
-    dataType: 'json'
-  });
-}
-
-
-function insertTweet(tweet) {
-
-  $('.tweet-media').html('');
-
-  $('.tweet-message').text(tweet.message);
-
-  if (tweet.media_url != "") {
-    $('.tweet-media').html("<img src='" + tweet.media_url + "'>");
-  }
-}
